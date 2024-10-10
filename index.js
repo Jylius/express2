907bb590f635f0bb09535ae99852bc5600623355
Jylius <orkun88.tokyo@gmail.com>
2024-10-10 10:34:31 +0300

    with body parser

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-//a simple middleware 
-app.use((req,res,next)=>{
-    console.log("you get new request : " + Date.now());
-    next(); // 
-})
 
-app.get('/',(req,res)=>{
-    res.send('Ana sayfa');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/data', (req, res) => {
+    console.log(req.body);
+    res.send("Gönderilen veriler alındı kardeşim!"); 
 });
 
-app.listen(3000, ()=>{
-    console.log("kardeş sorma artık la çalışıyorum");
-})
+// Basit bir middleware (isteğe bağlı)
+// app.use((req, res, next) => {
+//     console.log("Yeni bir istek alındı: " + Date.now());
+//     next(); 
+// });
+
+// Ana sayfa (isteğe bağlı)
+// app.get('/', (req, res) => {
+//     res.send('Ana sayfa');
+// });
+
+app.listen(3000, () => {
+    console.log("kardeş sorma artık la çalışıyorum.");
+});
