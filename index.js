01371205f122ff9b306d93455493119bf7019bd7
Jylius <orkun88.tokyo@gmail.com>
2024-10-15 08:58:54 +0300

    gereksizler arındırıldı
    oleeey

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-const things = require("./routes/things.js");
-app.use('/things',things);
-
-app.listen(3000, ()=>{
-   console.log('server is active on zort 3000');
-})
